7bd1d0d85c8aa5def80faa59422eec2abc5f9bfc
Shawn <sgiovanni1@yahoo.com>
2023-03-03 19:52:45 -0500

    created midiContext working

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { MidiProvider } from './Context/MidiProvider';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MidiProvider>
+      <App />
+    </MidiProvider>
+      
   </React.StrictMode>
 );
 
